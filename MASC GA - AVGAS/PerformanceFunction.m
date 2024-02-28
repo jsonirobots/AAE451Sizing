27d0b37614ddc16624189b31d2253fa83d0f64bf
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-02-28 02:33:40 -0500

    Update PerformanceFunction.m
    Changed for 4 jet engines

--- MASC GA - AVGAS/PerformanceFunction.m
+++ MASC GA - AVGAS/PerformanceFunction.m
@@ -4,14 +4,12 @@ function output = PerformanceFunction(inputs)
   Cl_to   = inputs.AeroInputs.Clmax/1.44; 
  
 % Compute takeoff parrameter (based on Raymer Ch.5 Figure 5.4)
-  takeoff_parameter = (inputs.PerformanceInputs.WS)/(Cl_to*inputs.PerformanceInputs.PW);
+  takeoff_parameter = (inputs.PerformanceInputs.WS)/(Cl_to*inputs.PerformanceInputs.TW);
   
 % Estimate takeoff distance by using takeoff_parameter and Figure 5.4 in Raymer Ch.5 
-  output.Sto = takeoff_parameter*8.5; % take-off distance; 8.5 is the slope for Ground Roll for a propeller aircraft;
+  output.Sto = (35793/1029250*takeoff_parameter+1129/20585) * 1000; % take-off distance; 8.5 is the slope for Ground Roll for a 4-engined jet aircraft;
                                       % change to meet FAR part 23
                                       % requirements
   
 %% See Raymer Ch. 17 to comptue other performance characteristics
 %% See Raymer Ch. 16 to compute control and stability performance
-  
-  
