547120203764ea9927844507229992448370d926
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-02-28 00:21:58 -0500

    Create DescentFunction.m
    Part from chapter 6 which was previously missing

---
+++ MASC GA - AVGAS/DescentFunction.m
@@ -0,0 +1,8 @@
+% Function that computes descent fuel weight fraction
+
+function output = DescentFunction(inputs)
+% Historical descent fuel weight fraction (Raymer Ch.6, Equation 6.22)
+% "inputs" not used here
+
+output.f_dec = 0.993;
+end
