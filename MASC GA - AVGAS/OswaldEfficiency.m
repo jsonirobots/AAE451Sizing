f5494745fa4c7ec18ec6760eafc54f616d25c936
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-02-28 01:09:23 -0500

    Update OswaldEfficiency.m
    Changed to use wing sweep

--- MASC GA - AVGAS/OswaldEfficiency.m
+++ MASC GA - AVGAS/OswaldEfficiency.m
@@ -10,8 +10,9 @@ AR     = inputs.GeometryInputs.AR;               % wing aspect  ratio
 Lambda = inputs.GeometryInputs.WingSweep*pi/180; % wing sweep [rad]
 b      = inputs.GeometryOutput.b;                % wing span [ft]
 Cdo    = inputs.Aero.Cdo;                        % Parasite drag coefficient
+
 %%
 %%
 
-e0 = 1.78*(1-0.045*AR^0.68)-0.64;
+e0 = 4.61*(1-0.045*AR^0.68)*(cos(Lambda))^0.15-3.1;
 end
