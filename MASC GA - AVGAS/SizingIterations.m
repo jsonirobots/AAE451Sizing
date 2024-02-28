251b9273b8c9c72ad811b3aa7ba4452f6b2d18fa
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-02-28 00:29:56 -0500

    Update SizingIterations.m
    Added descent segment

--- MASC GA - AVGAS/SizingIterations.m
+++ MASC GA - AVGAS/SizingIterations.m
@@ -4,7 +4,7 @@
 % The design mission is assumed to be:
 %
 %              ______cruise_____
-%             /                 \ descend
+%             /                 \ descent
 %            /                   \___
 %           /climb                \_/ loiter
 %          /                        \ 
@@ -39,30 +39,34 @@ while diff > tolerance
   
 % Warm-up and Takeoff segment fuel weight fraction
   WarmupTakeoffOutput = WarmupTakeoffFunction(inputs);
-  f_to                = WarmupTakeoffOutput.f_to;   % warm-up and takeoff fuel weight fraction
-  W1                  = TOGW_temp*f_to;             % aircraft weight after warm-up and takeoff [lbs]
+  f_to                = WarmupTakeoffOutput.f_to;         % warm-up and takeoff fuel weight fraction
+  W1                  = TOGW_temp*f_to;                   % aircraft weight after warm-up and takeoff [lbs]
 % Climb segment fuel weight fraction
   ClimbOutput         = ClimbFunction(inputs);
-  f_cl                = ClimbOutput.f_cl;           % climb fuel weight fraction
-  W2                  = W1*f_cl;                    % aircraft weight after climb segment [lbs]
+  f_cl                = ClimbOutput.f_cl;                 % climb fuel weight fraction
+  W2                  = W1*f_cl;                          % aircraft weight after climb segment [lbs]
 % Cruise segment fuel weight fraction
   CruiseOutput        = CruiseFunction(inputs,W2);
-  f_cr                = CruiseOutput.f_cr;          % cruise fuel weight fraction
-  W3                  = W2*f_cr;                    % aircraft weight after cruise segment [lbs]
+  f_cr                = CruiseOutput.f_cr;                % cruise fuel weight fraction
+  W3                  = W2*f_cr;                          % aircraft weight after cruise segment [lbs]
+% Descent segment fuel weight fraction
+  DescentOutput       = DescentFunction(inputs);
+  f_dec               = DescentOutput.f_dec;              % descent fuel weight segment
+  W4                  = W3*f_dec;                         % aircraft weight after descent segment [lbs]
 % Loiter segment fuel weight fraction
-  LoiterOutput        = LoiterFunction(inputs,W3);
-  f_lt                = LoiterOutput.f_lt;          % loiter fuel weight segment
-  W4                  = W3*f_lt;                    % aircraft weight after loiter segment [lbs]
+  LoiterOutput        = LoiterFunction(inputs,W4);
+  f_lt                = LoiterOutput.f_lt;                % loiter fuel weight segment
+  W5                  = W4*f_lt;                          % aircraft weight after loiter segment [lbs]
 % Landing and taxi fuel weight fraction
   LandingTaxiOutput   = LandingTaxiFunction(inputs);
-  f_lnd               = LandingTaxiOutput.f_lnd;    % landing and taxi fuel weight segment
-  W5                  = W4*f_lnd;                   % aircraft weight after landing & taxi segment [lbs]
+  f_lnd               = LandingTaxiOutput.f_lnd;          % landing and taxi fuel weight segment
+  W6                  = W5*f_lnd;                         % aircraft weight after landing & taxi segment [lbs]
 
 %% Compute new weights based on results of current iteration  
 % Total fuel weight fraction (including trapped fuel of 5%)
 
-  FWF       = 1.01*(1- f_to*f_cl*f_cr*f_lt*f_lnd);  % Fuel weight fraction 
-  Wfuel     = FWF*TOGW_temp;                        % Total fuel weight [lbs] (Overestimates - used scaling factor)
+  FWF       = 1.01*(1- f_to*f_cl*f_cr*f_dec*f_lt*f_lnd);  % Fuel weight fraction 
+  Wfuel     = FWF*TOGW_temp;                              % Total fuel weight [lbs] (Overestimates - used scaling factor)
   
 % Aircraft Takeoff Gross Weight Weight (TOGW) [lbs]: Wempty+Wpayload+Wfuel  
   TOGW      = EmptyWeightOutput.We + inputs.PayloadInputs.w_payload + Wfuel;  
