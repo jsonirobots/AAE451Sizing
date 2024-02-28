b262a63c6789caaa376a4736368d7a417d9826cf
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-02-28 00:13:26 -0500

    Update ClimbFunction.m
    Modified for jet aircraft

--- MASC GA - AVGAS/ClimbFunction.m
+++ MASC GA - AVGAS/ClimbFunction.m
@@ -1,8 +1,8 @@
-% Function that computes landing and taxi fuel weight fraction
+% Function that computes climb fuel weight fraction
 
 function output = ClimbFunction(inputs)
-% Historical climb fuel weight fraction (Raymer Ch.3, Table 3.2)
-% "inputs" not used here
+% Historical climb fuel weight fraction (Raymer Ch.6, Equation 6.8)
+  M         = inputs.PerformanceInputs.M;
 
-output.f_cl = 0.985;
+  output.f_cl = 1.0065-0.0325*M;
 end
