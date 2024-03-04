111b3f9711a82626e5a477b114158e847c9b5d98
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-03-04 11:49:32 -0500

    Update OswaldEfficiency.m
    Changed formula

--- MASC GA - AVGAS/OswaldEfficiency.m
+++ MASC GA - AVGAS/OswaldEfficiency.m
@@ -13,6 +13,6 @@ Cdo    = inputs.Aero.Cdo;                        % Parasite drag coefficient
 
 %%
 %%
-
-e0 = 4.61*(1-0.045*AR^0.68)*(cos(Lambda))^0.15-3.1;
+e0 = 1.78*(1-0.045*AR^0.68)-0.64;
+%e0 = 4.61*(1-0.045*AR^0.68)*(cos(Lambda))^0.15-3.1;
 end
