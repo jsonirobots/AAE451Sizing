4e6b0b5ecf4ee20b33c81288b1697b1abda981b9
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-03-04 07:21:59 -0500

    Update PerformanceFunction.m
    Now generates a constraint diagram for takeoff distance, landing distance, and top of climb

--- MASC GA - AVGAS/PerformanceFunction.m
+++ MASC GA - AVGAS/PerformanceFunction.m
@@ -1,16 +1,48 @@
 function FinalOutput = PerformanceFunction(inputs)
   FinalOutput             = inputs;
 
+  WSrange=linspace(10,200,100000);
+
 % Comptue takeoff lift coefficient (assumes Clmax is 1.2 times the takeoff lift coefficient 
-  Cl_to   = inputs.AeroInputs.Clmax/1.44; 
- 
-% Compute takeoff parrameter (based on Raymer Ch.5 Figure 5.4)
-  takeoff_parameter = (inputs.PerformanceInputs.WS)/(Cl_to*inputs.PerformanceInputs.TW);
+  Cl_to   = inputs.AeroInputs.Clmax/1.2; 
+  Cl_land = inputs.AeroInputs.Clmax;
+  WFcruise = inputs.W2overW0;
+  lapse = inputs.PropulsionInputs.lapse;
+  V  = inputs.PerformanceInputs.V;            % Velocity [knots]
+  h  = inputs.PerformanceInputs.hc;            % Altitude [ft]
+  WS = inputs.PerformanceInputs.WS;
+  AR = inputs.GeometryInputs.AR;
+  nmax = inputs.PerformanceInputs.nmax;
+
+  takeoff_parameter = (9-1129/20585)*1029250/35793;
+  TWto = WSrange/takeoff_parameter/Cl_to;
+
+  WSland = Cl_land/0.85/80*(9000-1000);
   
-% Estimate takeoff distance by using takeoff_parameter and Figure 5.4 in Raymer Ch.5 
-  FinalOutput.Sto = (35793/1029250*takeoff_parameter+1129/20585) * 1000; % take-off distance; 8.5 is the slope for Ground Roll for a 4-engined jet aircraft;
-                                      % change to meet FAR part 23
-                                      % requirements
+  [a,mu,rho] = AtmosphereFunction(h);
+  v = V*1.68781;
+  q = 0.5*rho*v^2;
+  Cdo = ParasiteDragFunction(inputs);
+  oswaldInputs = inputs;
+  oswaldInputs.Aero.Cdo = Cdo;
+  e0 = OswaldEfficiency(oswaldInputs);
+
+
+  TWtoc = WFcruise/lapse*(q/WFcruise*(Cdo./WSrange+WSrange./pi./AR./e0*(nmax*WFcruise/q)^2)+100/60/v);
+  
+  TWmax=max([TWto,TWtoc]);
+  TWmin=min([TWto,TWtoc]);
+
+  plot(WSrange, TWto)
+  hold on
+  plot(WSrange, TWtoc)
+  plot([WSland,WSland],[TWmin,TWmax])
+  hold off
+  grid on
+  legend("Takeoff","Top of Climb","Landing","Location","best")
+  xlabel("Wing Loading (lb/ft^2)")
+  ylabel("Thrust to Weight Ratio")
+
   
 %% See Raymer Ch. 17 to comptue other performance characteristics
 %% See Raymer Ch. 16 to compute control and stability performance
