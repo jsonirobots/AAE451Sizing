2f16d6e054d5ddc326a8baabdea94cac229e3634
jojowoj <161868919+jojowoj@users.noreply.github.com>
2024-03-05 13:34:09 -0500

    Update AircraftParameters.m
    Changes to fuselage length, lapse rate, cl_max, fuselage diameter, and max load factor.
    
    H-tail and V-tail comment updates, I guess?

--- MASC GA - AVGAS/AircraftParameters.m
+++ MASC GA - AVGAS/AircraftParameters.m
@@ -6,7 +6,7 @@ PerformanceInputs.WS   = 840000/6200;                         % wing loading [lb
 PerformanceInputs.V    = 533.391;                                 % cruise velocity [knots]
 PerformanceInputs.M    = 0.8;                                % cruise velocity [Mach]. This needs to be changed to match V at desired altitude.  Can automate this calculation with the AtmosphereFunction
 PerformanceInputs.Vlt  = 266.985;                                 % loiter velocity [knots]
-PerformanceInputs.nmax = 2;                                   % maximum load factor
+PerformanceInputs.nmax = 3;                                   % maximum load factor
 PerformanceInputs.hc   = 31000;                               % cruise altitude [ft]
 PerformanceInputs.hlt  = 25000;                               % loiter altitude [ft]
 
@@ -18,21 +18,21 @@ GeometryInputs.TR          = 0.4286;          % wing taper ratio
 GeometryInputs.Cht         = 1.00;         % Horizontal-tail volume coefficient (based on Raymer Ch.6 Table 6.4)
 GeometryInputs.Cvt         = 0.08;         % Vertical-tail volume coefficient (based on Raymer Ch.6 Table 6.4)
 GeometryInputs.H_loc       = 0.45;         % Location of H-tail as a fraction of fuselage length
-GeometryInputs.H_loc       = 0.45;         % Location of H-tail as a fraction of fuselage length
+GeometryInputs.H_loc       = 0.45;         % Location of V-tail as a fraction of fuselage length
 GeometryInputs.le          = 224/12;       % engine length [ft]
 GeometryInputs.de          = 163.7/12;     % engine diameter [ft]
 
 %% CONFIGURATION PARAMETERS
-LayoutInputs.lf = 300;                      % length of fuselage [ft]
+LayoutInputs.lf = 250.6562;                      % length of fuselage [ft]
 LayoutInputs.df = 26;                     % diameter of fuselage [ft]
 
 %% AERODYNAMIC PARAMETERS
-AeroInputs.Clmax   = 1.6;                  % maximum lift coefficient
+AeroInputs.Clmax   = 1.2;                  % maximum lift coefficient
 
 %% PROPULSION PARAMETERS
 PropulsionInputs.num_eng    = 4;           % number of engines
 PropulsionInputs.C          = 0.5;         % Jet specific fuel consumption [1/hr] 
-PropulsionInputs.lapse      = 0.35;           % lapse rate of engines at cruise
+PropulsionInputs.lapse      = 0.25;           % lapse rate of engines at cruise
 
 inputs.PerformanceInputs = PerformanceInputs;
 inputs.GeometryInputs    = GeometryInputs;
