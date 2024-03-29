f875bb9611f7807784c246bca4b49993699609fe
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2024-03-29 15:44:55 -0400

    Update GeometryFunction.m
    Support for carpet plots

--- MASC GA - AVGAS/GeometryFunction.m
+++ MASC GA - AVGAS/GeometryFunction.m
@@ -35,25 +35,25 @@ de          = inputs.GeometryInputs.de;                       % engine diameter 
 
 %%
 %% Wing geometry computations (See Raymer Ch.7 Eq. 7.5-7.8)
-Sw          = Wt/WingLoading;                          % Wing planform area [ft^2]
-b           = sqrt(AR*Sw);                             % Wing span [ft]
-MAC         = ((1+TR+(TR^2))/(1+TR)^2)*(Sw/b)*(4/3);   % Mean aerodynamic chord of wing
+Sw          = Wt./WingLoading;                          % Wing planform area [ft^2]
+b           = sqrt(AR.*Sw);                             % Wing span [ft]
+MAC         = ((1+TR+(TR.^2))./(1+TR).^2).*(Sw./b).*(4/3);   % Mean aerodynamic chord of wing
 Swetwing    = 2*Sw*1.02;                               % Wing wetted area [ft^2]
 %% Fuselage wetted computations
 % need to be checked for source
-fr          = lf/df;                                   % fuselage finess ratio
-Swetfus     = pi*df*lf*(1-2/fr)^(2/3)*(1+1/fr^2);      % wetted area of fuselage [ft^2]
+fr          = lf./df;                                   % fuselage finess ratio
+Swetfus     = pi.*df.*lf.*(1-2./fr).^(2/3)*(1+1./fr.^2);      % wetted area of fuselage [ft^2]
 
 %% Tails geometry computations (Based on Raymer Ch.6 Eq. 6.28-6.29)
-Lht         = H_loc*lf;                                % H-tail moment arm [ft]
-Lvt         = V_loc*lf;                                % V-tail moment arm (for engines on wing) [ft]
-Sv          = Cvt*b*Sw/Lvt;                            % V-tail surface area [ft^2]
-Sh          = Cht*MAC*Sw/Lht;                          % H-tail surface area [ft^2]
+Lht         = H_loc.*lf;                                % H-tail moment arm [ft]
+Lvt         = V_loc.*lf;                                % V-tail moment arm (for engines on wing) [ft]
+Sv          = Cvt.*b.*Sw./Lvt;                            % V-tail surface area [ft^2]
+Sh          = Cht.*MAC.*Sw./Lht;                          % H-tail surface area [ft^2]
 Sweth       = 2*Sh*1.02;                               % H-tail wetted area [ft^2]
 Swetv       = 2*Sv*1.02;                               % V-tail wetted area [ft^2]
 
 %% Engine geometry computations (based on Raymer Taqble 10.4)
-Sweteng     = pi*de*le*num_eng;                        % wetted area of engines [ft^2]
+Sweteng     = pi.*de.*le.*num_eng;                        % wetted area of engines [ft^2]
 
 %% Total wetted area computation
 Swet        = Swetwing+Swetfus+Swetv+Sweth+Sweteng;    % total wetted area of aircraft [ft^2]
